47fff009698cdd8653ab6352948ad1a03984915f
sanjayapandey <pandeysanjaya4@gmail.com>
2022-12-29 14:44:13 -0500

    first kafka consumer commit

---
+++ src/main/java/com/sanjaya/kafka/consumer/KafkaConsumer.java
@@ -0,0 +1,18 @@
+package com.sanjaya.kafka.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    @KafkaListener(topics = "wikimedia_recentchange", groupId = "myGroup")
+    public void consume(String eventMessage){
+        LOGGER.info(String.format("Event message received -> %s", eventMessage));
+
+    }
+}
